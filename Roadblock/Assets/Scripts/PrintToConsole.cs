f51e7faad439b0e165ca0a960df373f6501e51bf
Zilox <johannes.grcevic65@gmail.con>
2023-03-10 22:11:30 +0000

    Upload project files.

---
+++ Roadblock/Assets/Scripts/PrintToConsole.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Someone put a road closed sign in the middle of the highway and now all the cars are stuck.");
+    }
+}
